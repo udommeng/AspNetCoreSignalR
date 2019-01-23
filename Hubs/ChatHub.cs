ba4cd177ad8824d6cace766b7fd838719d1564e0
Meng-MSI <udommeng@gmail.com>
2019-01-23 21:06:54 +0700

    Chart เสร็จเรียบร้อย

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -1,9 +1,10 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 
 namespace AspNetCoreSignalR.Hubs
 {
     public class ChatHub : Hub
     {
-        
+        public async Task SendMessageToHub(string user, string message) => await Clients.All.SendAsync("ReceiveMessage", user, message);
     }
 }
